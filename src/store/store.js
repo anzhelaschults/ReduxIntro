3b4310731350a5e16720270068afb67a0527e35c
Anzhela Schults <anzhela.schults@gmail.com>
2021-11-15 22:04:40 +0100

    upd

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,10 @@ import reducer from "./reducer";
 //reducer should be an argument
 // reducer - function where 2 things come in (state and action) and 1 come out (state)
 
+//The place where we keep our state - the "store"
+
+// Method createStore() creates a store
+
 const store = createStore( reducer );
 
 export default store;
